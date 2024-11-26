7d2a79189d5dc920eb3c59ca8da0f0d5747e9bec
VitaFrancesco <vitafrancesco00@gmail.com>
2024-11-26 15:13:34 +0100

    ADD: division of completed tasks from incomplete ones showing them in the dom

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import tasks from '../data/tasks.js'
 
 function App() {
-
+  const completedTasks = tasks.filter((el) => el.state === 'completed');
+  const currentTasks = tasks.filter((el) => el.state !== 'completed');
   return (
     <>
-
+      <header>
+        <nav className='container'>
+          <h1>Task Manager</h1>
+        </nav>
+      </header>
+      <main className='container'>
+        <section>
+          <h2>Current Tasks</h2>
+          <ul>
+            {currentTasks.map((el, id) => (
+              <li key={el.id}>{el.title}</li>
+            ))}
+          </ul>
+        </section>
+        <hr />
+        <section>
+          <h2>Completed Tasks</h2>
+          <ul>
+            {completedTasks.map((el, id) => (
+              <li key={el.id}>{el.title}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </>
   )
 }
